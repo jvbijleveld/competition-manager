621051549c087fbc6124fe132a24c27314d4e1fd
Jeffrey@Cloud9 <jeffrey@vanbijleveld.nl>
2018-02-06 12:51:07 +0000

    struggling with spring security

--- src/main/java/nl/vanbijleveld/cm/auth/WebSecurityConfig.java
+++ src/main/java/nl/vanbijleveld/cm/auth/WebSecurityConfig.java
@@ -19,9 +19,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
+        http.authorizeRequests().antMatchers("/", "/home").permitAll().anyRequest().authenticated().and().formLogin().loginPage("/login").permitAll().and().logout().permitAll();
 
-        http.authorizeRequests().anyRequest().permitAll();
+        //http.authorizeRequests().anyRequest().permitAll();
     }
 
 }
